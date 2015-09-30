afdf3df06dfd59d9563dc2ed45ff5677d5e8e902
achewink <achewink@gmail.com>
2015-09-30 15:56:19 +0800

    minor improvement

--- _site/dist/js/bootstrap.js
+++ _site/dist/js/bootstrap.js
@@ -900,7 +900,7 @@ if (typeof jQuery === 'undefined') {
   // ===================================
 
   $(document)
-    .on('click.bs.dropdown.data-api', clearMenus)
+    .on('click.bs.dropdown.data-api', toggle, Dropdown.prototype.toggle)
     .on('mouseover.bs.dropdown.data-api', '.dropdown form', function (e) { e.stopPropagation() })
     .on('mouseover.bs.dropdown.data-api', toggle, Dropdown.prototype.toggle)
     .on('keydown.bs.dropdown.data-api', toggle, Dropdown.prototype.keydown)
